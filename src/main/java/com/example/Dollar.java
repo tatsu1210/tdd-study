b1611628dac6402fdde26d54d6326e013d70efe6
Tatsuhiko Matsuda <oregamatsuda@gmail.com>
2026-05-10 15:41:07 +0900

    Complete Chapter2

---
+++ src/main/java/com/example/Dollar.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
