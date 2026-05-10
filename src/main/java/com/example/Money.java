5c1fcbc2756c2cc173a6644d1477a283d883f0ce
Tatsuhiko Matsuda <oregamatsuda@gmail.com>
2026-05-10 23:37:32 +0900

    Complete Chapter8

--- src/main/java/com/example/Money.java
+++ src/main/java/com/example/Money.java
@@ -1,10 +1,22 @@
 package com.example;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return this.amount == money.amount;
+        return this.amount == money.amount
+                && getClass().equals(money.getClass());
     }
+
+    static Money Dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money Franc(int amount) {
+        return new Franc(amount);
+    }
+
 }
